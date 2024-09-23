5ff5cb05b297235c09e6e09dafc4ac128421cfbf
Yago <dolmayan@gmail.com>
2024-09-23 20:46:38 +0200

    Add first version of add, deduct, multiply, divide functions

---
+++ script.js
@@ -0,0 +1,19 @@
+// function to add
+function add(a, b) {
+    return a + b;
+}
+
+// function to deduct
+function deduct(a, b) {
+    return a - b;
+}
+
+// function to multiply
+function multiply(a, b) {
+    return a * b;
+}
+
+// function to divide
+function divide(a, b) {
+    return a / b;
+}
