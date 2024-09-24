172f06306a0afad2ace78cb632d847bb50f919e5
Yago <dolmayan@gmail.com>
2024-09-24 10:20:27 +0200

    Create first draft of calculator HTML layout

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// vars
+let number1 = 0;
+let number2 = 0;
+let total = 0;
+
 // function to add
 function add(a, b) {
     return a + b;
 }
 
-// function to deduct
-function deduct(a, b) {
+// function to subtract
+function subtract(a, b) {
     return a - b;
 }
 
@@ -16,4 +21,13 @@ function multiply(a, b) {
 // function to divide
 function divide(a, b) {
     return a / b;
+}
+
+// function to handle operations, total and variables
+function operate(a, b, operation) {
+    let number1 = a;
+    let number2 = b;
+    let total = 0;
+    total = operation(number1, number2);
+    return total;
 }
