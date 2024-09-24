adfacfdc96e5467dfbf797a0eb670166f76caee3
Yago <dolmayan@gmail.com>
2024-09-24 21:21:29 +0200

    Add digit input

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-// vars
+// TESTING
 let number1 = 0;
 let number2 = 0;
 let total = 0;
+
+// Add event listener to all digit buttons, to update the display
+const digitButtons = document.querySelectorAll(".digit-button");
+// Add function to update the display with the appropiate number
+digitButtons.forEach(function(e) {
+    e.addEventListener("click", function() {
+        updateDisplay(e.innerHTML);
+    })
+})
+    
 
 // function to add
 function add(a, b) {
@@ -27,7 +37,26 @@ function divide(a, b) {
 function operate(a, b, operation) {
     let number1 = a;
     let number2 = b;
-    let total = 0;
-    total = operation(number1, number2);
+    
+    let total = operation(number1, number2);
     return total;
 }
+
+function updateDisplay(a) {
+    // grab display html content
+    // update html content to a
+    let displayString = document.querySelector("#display-string");
+
+    // If current number is 0, we don't want to keep it
+    if (displayString.innerHTML != "0") {
+        displayString.innerHTML = displayString.innerHTML + a;
+    } else {
+        displayString.innerHTML = a;
+    }
+    
+}
+
+// start state: display is 0 (number1 = 0). If 1 is pressed, number1 = 1, number keeps being defined until operator button is pressed
+
+// what if I create an object, num1, num2, result that keeps being reset?
+
